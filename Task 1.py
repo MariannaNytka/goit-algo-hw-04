bd344ca6db4524d2fbe128ea65d3486c0f6020a2
MariannaNytka <mariannanytka@gmail.com>
2024-05-04 19:25:12 +0200

    Task_01

--- Task 1.py
+++ Task 1.py
@@ -1,37 +1,36 @@
-fh=open('salary.txt','w')
-symbols_written = fh.write('Alex Korp,3000\nNikita Borisenko,2000\nSitarama Raju,1000')
+import sys
 
 def total_salary(path):
     try:
-        total_salary=0
+        total_salary = 0
         num_developers = 0
-        with open(path, 'r', encoding = 'utf-8') as file:
+        with open(path, 'r', encoding='utf-8') as file:
             for line in file:
-                data=line.strip().spilt(',')
-                if len(data)==2:
-                    salary=int(data[1])
-                    total_salary +=salary
-                    num_developers +=1
+                data = line.strip().split(',')
+                if len(data) == 2:
+                    try:
+                        salary = int(data[1])
+                    except ValueError:
+                        print(f"Error: Invalid salary format in line: {line}", file=sys.stderr)
+                        continue
+                    total_salary += salary
+                    num_developers += 1
                 else:
-                    print(f"Error in formate line :{line}")
-            if num_developers>0:
-                average_salary = total_salary/num_developers
+                    print(f"Error: Invalid format in line: {line}", file=sys.stderr)
+            if num_developers > 0:
+                average_salary = total_salary / num_developers
                 return total_salary, average_salary
             else:
-                return 0,0
+                return 0, 0
     except FileNotFoundError:
-        print("File is not found")
-        return 0,0
+        print("Error: File is not found", file=sys.stderr)
+        return 0, 0
     except Exception as e:
-        print(f"Error :{e}")
-        return 0,0
- 
- from pathlib import Path
- path= Path("goit-algo-hw-04/salary.txt")
- abs_path=path.absolute()
- print(abs_path)  
+        print(f"Error: {e}", file=sys.stderr)
+        return 0, 0
 
-total, average = total_salary("goit-algo-hw-04/to/salary.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+total, average = total_salary("C:\\Users\\maria\\OneDrive\\Documents\\GitHub\\goit-algo-hw-04\\salary.txt")
+print(f"Total salary: {total}, Average salary: {average}")
+
    
 
